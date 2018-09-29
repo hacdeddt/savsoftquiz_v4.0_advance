95ab8725a8cc6047b13da0c288dc0494e06ce218
hacdeddt <hacdeddt3@gmail.com>
2018-09-30 01:04:23 +0700

    edit validate for password

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -224,7 +224,6 @@ class Login extends CI_Controller {
 	
 	}
 	
-	
 		public function insert_user()
 	{
 		
@@ -233,7 +232,7 @@ class Login extends CI_Controller {
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules('email', 'Email', 'required|is_unique[savsoft_users.email]');
 		$this->form_validation->set_rules('studentCode', 'Student Code', 'required|is_unique[savsoft_users.student_code]');
-        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|alpha_numeric');
           if ($this->form_validation->run() == FALSE)
                 {
                      $this->session->set_flashdata('message', "<div class='alert alert-danger'>".validation_errors()." </div>");
