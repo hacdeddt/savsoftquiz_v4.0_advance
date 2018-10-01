4a765c7234916fe026b0f92490dba7c0759a4922
hacdeddt <hacdeddt3@gmail.com>
2018-10-01 20:04:42 +0700

    update config

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
-<?php 
-		$sq_base_url='http://localhost:8181/quiz/';
-		$sq_hostname='localhost';
-		$sq_dbname='sqtry2';
-		$sq_dbusername='root';
-		$sq_dbpassword='';
-		?>
+<?php
+		$sq_base_url='http://35.196.131.192/quiz/';
+        $sq_hostname='localhost';
+        $sq_dbname='quiz';
+        $sq_dbusername='quiz';
+        $sq_dbpassword='quiz';
+        ?>
